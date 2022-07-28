109b12be8d91b155632be78ed5a366990a5bda7a
Brendan Miller <bmill3r@gmail.com>
2022-07-28 15:07:48 -0400

    expanded bullets and easier to swap in different resume templates

--- render_resume.r
+++ render_resume.r
@@ -12,14 +12,16 @@
 ## set the working directory to this repo.
 root_dir <- paste0(getwd(), "/output/")
 
+resume_template <- "my_resume.rmd"
+
 # Knit the HTML version
-rmarkdown::render(paste0(root_dir, "my_resume.rmd"),
+rmarkdown::render(paste0(root_dir, resume_template),
                   params = list(pdf_mode = FALSE),
                   output_file = paste0(root_dir, "BrendanMillerResume.html"))
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
-rmarkdown::render(paste0(root_dir, "my_resume.rmd"),
+rmarkdown::render(paste0(root_dir, resume_template),
                   params = list(pdf_mode = TRUE),
                   output_file = tmp_html_cv_loc)
 
